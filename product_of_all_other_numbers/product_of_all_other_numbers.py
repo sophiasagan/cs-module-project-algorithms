4500fe46a730d43d15af1c3be955590dc19d9f37
Sophia Sagan <sophiabraddock82@gmail>
2020-07-14 17:52:34 -0400

    product_of_ all...  done

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -2,15 +2,37 @@
 Input: a List of integers
 Returns: a List of integers
 '''
+import math
+
 def product_of_all_other_numbers(arr):
     # Your code here
+    prod_arr = []
 
-    pass
+    for i in range(len(arr)):
+        product = 1
+        for element in arr:
+            product *= element
+            # print(product)
+        product /= arr[i]
+        # print(product)
+        prod_arr.append(product)
+        
+    return prod_arr
+
+def product_of_all_other_numbers2(arr):
+    # Your code here
+    prod_arr = []
+
+    for i in range(len(arr)):
+        prod_arr.append(math.prod(arr[:i] + arr[i+1:]))
+    return prod_arr
+
 
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
-    # arr = [1, 2, 3, 4, 5]
-    arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
+    arr = [1, 2, 3, 4, 5]
+    # arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
 
     print(f"Output of product_of_all_other_numbers: {product_of_all_other_numbers(arr)}")
+    print(f"Output of product_of_all_other_numbers: {product_of_all_other_numbers2(arr)}")
