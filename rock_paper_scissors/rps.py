329b6df676f01bdbe881092352a161711bf63fb3
Sophia Sagan <sophiabraddock82@gmail>
2020-07-16 19:14:02 -0400

    sliding window ah and rps??

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,20 +2,38 @@
 
 import sys
 import itertools
+from itertools import combinations, product, permutations, combinations_with_replacement
 
 choices = [['rock'], ['paper'], ['scissors']]
+# all_combos = [[]]
 
-def rock_paper_scissors(num_plays):
-  if 
+def rock_paper_scissors(n):
+  if n == 0:
+    return [[]]
+  elif n == 1:
+    return choices
+  else:
+    all_combos = itertools.product(choices, repeat=n)
+    return list(all_combos)
+    
+    # for _ in range(n, len(choices)+1):
+    
+    # all_combos = list(combinations_with_replacement(choices, n))
+    # # print(len(all_combos), all_combos)
+    # return all_combos
+  # for i in range(0, len(choices)+1):
+  #   for subset in itertools.combinations(choices, i):
+  #     return(subset)
+  
+print(rock_paper_scissors(2))
 
-  for i in range(0, len(choices)+1):
-    for subset in itertools.combinations(choices, i):
-      return(subset)
+
+
   
 
-if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+# if __name__ == "__main__":
+#   if len(sys.argv) > 1:
+#     n = int(sys.argv[1])
+#     print(rock_paper_scissors(n))
+#   else:
+#     print('Usage: rps.py [n]')
